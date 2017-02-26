e16a8c8c841bc0f5de68111de4f716fec8833c74
LIU-CS-CLUB <LIUcompsci.club@gmail.com>
2017-02-26 16:11:38 -0500

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -19,7 +19,7 @@
   modified 8 Sep 2016
   by Colby Newman
 */
-#define LED 13
+#define LED 13              //Pin 13 + ground
 
 // the setup function runs once when you press reset or power the board
 void setup() {
